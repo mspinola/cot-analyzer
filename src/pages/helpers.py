c8d45a8143418c9447450cbda25782d13a27935c
Matt Spinola <matt.spinola@gmail.com>
2026-05-05 22:32:09 -0400

    bug fix of vertical spacing

--- src/pages/helpers.py
+++ src/pages/helpers.py
@@ -195,7 +195,7 @@ def get_setup_highlighting(fig, df, min_threshold, max_threshold, row, col):
     return fig
 
 def get_make_subplots_for_plots(rows, cols, titles, specs):
-    vertical_spacing = min( (1 / (rows - 1)), const.VERTICAL_SPACING)
+    vertical_spacing = min( (1 / (rows - 1)), const.VERTICAL_SPACING) if rows > 1 else const.VERTICAL_SPACING
     fig = make_subplots(
         rows=rows,
         cols=cols,
